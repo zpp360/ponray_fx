15dddc2298be6e49446f638f8b11fae76cb761ce
zpp360 <184458308@qq.com>
2020-01-06 17:52:22 +0800

    menuBar

--- src/com/ponray/main/Main.java
+++ src/com/ponray/main/Main.java
@@ -38,13 +38,24 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         //力 显示框布局
-        Label lableNum1 = new Label("0.00");
+        Label lableNum1 = new Label("0.00000");
         lableNum1.setMinSize(165,55);
-        lableNum1.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 50));
+        lableNum1.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 40));
         lableNum1.setAlignment(Pos.CENTER);
         lableNum1.setStyle("-fx-background-color: #002060;");
-        lableNum1.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        lableNum1.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
         lableNum1.setTextFill(Color.web("#fff"));
+
+
+        //力峰值
+        Label labelTop = new Label("0.00");
+        labelTop.setMinSize(65,55);
+        lableNum1.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 40));
+        lableNum1.setAlignment(Pos.CENTER);
+        lableNum1.setStyle("-fx-background-color: #002060;");
+        lableNum1.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(0,0,1,1),Insets.EMPTY)));
+        lableNum1.setTextFill(Color.web("#fff"));
+
 
 
         Label labelN1 = new Label("N");
@@ -53,7 +64,7 @@ public class Main extends Application {
         labelN1.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelN1.setTextFill(Color.web("#fff"));
         labelN1.setAlignment(Pos.CENTER);
-        labelN1.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelN1.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         Label labelName1 = new Label("力");
         labelName1.setMinSize(68,23);
@@ -61,7 +72,7 @@ public class Main extends Application {
         labelName1.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelName1.setTextFill(Color.web("#fff"));
         labelName1.setAlignment(Pos.CENTER);
-        labelName1.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelName1.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         VBox vBox1 = new VBox();
         //调整上下组件间距
@@ -70,7 +81,7 @@ public class Main extends Application {
         vBox1.getChildren().addAll(labelN1,labelName1);
 
         Button button1 = new Button("清零");
-        button1.setMinSize(34,53);
+        button1.setMinSize(40,53);
 
         GridPane topGrid1 = new GridPane();
         //是否显示表格边框
@@ -78,14 +89,18 @@ public class Main extends Application {
         topGrid1.add(lableNum1,0,0,1,2);
         topGrid1.add(vBox1,1,0);
         topGrid1.add(button1,2,0,1,2);
+        topGrid1.setPadding(new Insets(5));
+        topGrid1.setBorder(new Border(new BorderStroke(Color.rgb(213,223,229), BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT,new Insets(10))));
+        //lableNum1.prefWidthProperty().bind(topGrid1.widthProperty());
+
 
         //位移显示框布局
-        Label lableNum2 = new Label("0.000");
+        Label lableNum2 = new Label("0.00000");
         lableNum2.setMinSize(165,55);
-        lableNum2.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 50));
+        lableNum2.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 40));
         lableNum2.setAlignment(Pos.CENTER);
         lableNum2.setStyle("-fx-background-color: #002060;");
-        lableNum2.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        lableNum2.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
         lableNum2.setTextFill(Color.web("#fff"));
 
 
@@ -95,7 +110,7 @@ public class Main extends Application {
         labelN2.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelN2.setTextFill(Color.web("#fff"));
         labelN2.setAlignment(Pos.CENTER);
-        labelN2.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelN2.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         Label labelName2 = new Label("位移");
         labelName2.setMinSize(68,23);
@@ -103,7 +118,7 @@ public class Main extends Application {
         labelName2.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelName2.setTextFill(Color.web("#fff"));
         labelName2.setAlignment(Pos.CENTER);
-        labelName2.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelName2.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         VBox vBox2 = new VBox();
         //调整上下组件间距
@@ -112,7 +127,7 @@ public class Main extends Application {
         vBox2.getChildren().addAll(labelN2,labelName2);
 
         Button button2 = new Button("清零");
-        button2.setMinSize(34,53);
+        button2.setMinSize(40,53);
 
         GridPane topGrid2 = new GridPane();
         //是否显示表格边框
@@ -120,14 +135,16 @@ public class Main extends Application {
         topGrid2.add(lableNum2,0,0,1,2);
         topGrid2.add(vBox2,1,0);
         topGrid2.add(button2,2,0,1,2);
+        topGrid2.setPadding(new Insets(5));
+        topGrid2.setBorder(new Border(new BorderStroke(Color.rgb(213,223,229), BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT,new Insets(10))));
 
         //变形显示框布局
         Label lableNum3 = new Label("0.00000");
         lableNum3.setMinSize(165,55);
-        lableNum3.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 50));
+        lableNum3.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 40));
         lableNum3.setAlignment(Pos.CENTER);
         lableNum3.setStyle("-fx-background-color: #002060;");
-        lableNum3.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        lableNum3.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
         lableNum3.setTextFill(Color.web("#fff"));
 
 
@@ -137,7 +154,7 @@ public class Main extends Application {
         labelN3.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelN3.setTextFill(Color.web("#fff"));
         labelN3.setAlignment(Pos.CENTER);
-        labelN3.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelN3.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         Label labelName3 = new Label("变形");
         labelName3.setMinSize(68,23);
@@ -145,7 +162,7 @@ public class Main extends Application {
         labelName3.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelName3.setTextFill(Color.web("#fff"));
         labelName3.setAlignment(Pos.CENTER);
-        labelName3.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelName3.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         VBox vBox3 = new VBox();
         //调整上下组件间距
@@ -154,7 +171,7 @@ public class Main extends Application {
         vBox3.getChildren().addAll(labelN3,labelName3);
 
         Button button3 = new Button("清零");
-        button3.setMinSize(34,53);
+        button3.setMinSize(40,53);
 
         GridPane topGrid3 = new GridPane();
         //是否显示表格边框
@@ -162,14 +179,16 @@ public class Main extends Application {
         topGrid3.add(lableNum3,0,0,1,2);
         topGrid3.add(vBox3,1,0);
         topGrid3.add(button3,2,0,1,2);
+        topGrid3.setPadding(new Insets(5));
+        topGrid3.setBorder(new Border(new BorderStroke(Color.rgb(213,223,229), BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT,new Insets(10))));
 
         //时间显示框布局
-        Label lableNum4 = new Label("0.00");
+        Label lableNum4 = new Label("0.00000");
         lableNum4.setMinSize(165,55);
-        lableNum4.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 50));
+        lableNum4.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 40));
         lableNum4.setAlignment(Pos.CENTER);
         lableNum4.setStyle("-fx-background-color: #002060;");
-        lableNum4.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        lableNum4.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
         lableNum4.setTextFill(Color.web("#fff"));
 
 
@@ -179,7 +198,7 @@ public class Main extends Application {
         labelN4.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelN4.setTextFill(Color.web("#fff"));
         labelN4.setAlignment(Pos.CENTER);
-        labelN4.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelN4.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         Label labelName4 = new Label("时间");
         labelName4.setMinSize(68,23);
@@ -187,7 +206,7 @@ public class Main extends Application {
         labelName4.setFont(Font.font(FontUtil.FANGSONG, FontWeight.NORMAL, 20));
         labelName4.setTextFill(Color.web("#fff"));
         labelName4.setAlignment(Pos.CENTER);
-        labelName4.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        labelName4.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         VBox vBox4 = new VBox();
         //调整上下组件间距
@@ -196,7 +215,7 @@ public class Main extends Application {
         vBox4.getChildren().addAll(labelN4,labelName4);
 
         Button button4 = new Button("清零");
-        button4.setMinSize(34,53);
+        button4.setMinSize(40,53);
 
         GridPane topGrid4 = new GridPane();
         //是否显示表格边框
@@ -204,14 +223,12 @@ public class Main extends Application {
         topGrid4.add(lableNum4,0,0,1,2);
         topGrid4.add(vBox4,1,0);
         topGrid4.add(button4,2,0,1,2);
+        topGrid4.setPadding(new Insets(5));
+        topGrid4.setBorder(new Border(new BorderStroke(Color.rgb(213,223,229), BorderStrokeStyle.SOLID, new CornerRadii(5), BorderWidths.DEFAULT,new Insets(10))));
 
         HBox hBox = new HBox();
-        hBox.setSpacing(30);
+        hBox.setSpacing(5);
         hBox.getChildren().addAll(topGrid1,topGrid2,topGrid3,topGrid4);
-        HBox.setHgrow(topGrid1,Priority.ALWAYS);
-        HBox.setHgrow(topGrid2,Priority.ALWAYS);
-        HBox.setHgrow(topGrid3,Priority.ALWAYS);
-        HBox.setHgrow(topGrid4,Priority.ALWAYS);
 
 
 
@@ -275,7 +292,7 @@ public class Main extends Application {
         tabPane.setMaxSize(TabPane.USE_PREF_SIZE,TabPane.USE_PREF_SIZE);
         tabPane.getTabs().addAll(tab1,tab2,tab3,tab4);
         tabPane.setStyle("-fx-tab-min-width: 225px;-fx-tab-max-width: 300px;-fx-tab-min-height: 35px;");
-        tabPane.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,new Insets(1,0,0,1))));
+        tabPane.setBorder(new Border(new BorderStroke(Color.rgb(160,160,160), BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT,Insets.EMPTY)));
 
         tab1.setClosable(false);
         tab1.setStyle("-fx-font-size:20px;");
@@ -287,21 +304,65 @@ public class Main extends Application {
         tab4.setClosable(false);
         tab4.setStyle("-fx-font-size:20px;");
 
+        //菜单
+        MenuBar menuBar = createMenuBar();
 
         //整体布局
+        VBox root = new VBox();
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(hBox);
+        hBox.prefWidthProperty().bind(borderPane.widthProperty());
         borderPane.setRight(rightGrid);
         borderPane.setCenter(tabPane);
+        borderPane.setPadding(new Insets(5,0,0,0));
+        root.getChildren().addAll(menuBar,borderPane);
 
-        Scene scene = new Scene(borderPane);
+        //宽度计算
+        menuBar.prefWidthProperty().bind(root.widthProperty());                        //绑定菜单宽度
+        borderPane.prefWidthProperty().bind(root.widthProperty());                     //绑定方位布局宽度
+        hBox.prefWidthProperty().bind(borderPane.widthProperty());//绑定顶部四个数显布局宽度
+        topGrid1.prefWidthProperty().bind(hBox.widthProperty().divide(4));//绑定第1个数显宽度
+        topGrid2.prefWidthProperty().bind(hBox.widthProperty().divide(4));//绑定第2个数显宽度
+        topGrid3.prefWidthProperty().bind(hBox.widthProperty().divide(4));//绑定第3个数显宽度
+        topGrid4.prefWidthProperty().bind(hBox.widthProperty().divide(4));//绑定第4个数显宽度
+        lableNum1.prefWidthProperty().bind(hBox.widthProperty().subtract(120));//绑定力显示屏宽度
+        lableNum2.prefWidthProperty().bind(hBox.widthProperty().subtract(120));//绑定位移显示屏宽度
+        lableNum3.prefWidthProperty().bind(hBox.widthProperty().subtract(120));//绑定变形显示屏宽度
+        lableNum4.prefWidthProperty().bind(hBox.widthProperty().subtract(120));//绑定时间显示屏宽度
+
+        Scene scene = new Scene(root);
         stage.setTitle("拉力试验工具软件");
         stage.setMinWidth(1000);
         stage.setMinHeight(700);
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/user.jpg")));
         stage.setScene(scene);
-        stage.setResizable(false);
+        //控制窗口是否可以缩放
+        stage.setResizable(true);
         stage.show();
+
+    }
+
+
+    private MenuBar createMenuBar(){
+
+        MenuBar menuBar = new MenuBar();
+
+        Menu language = new Menu("语言");
+        ToggleGroup languageGroup = new ToggleGroup();
+        RadioMenuItem chinaItem = new RadioMenuItem("中文");
+        chinaItem.setSelected(true);
+        chinaItem.setToggleGroup(languageGroup);
+        RadioMenuItem englishItem = new RadioMenuItem("English");
+        englishItem.setToggleGroup(languageGroup);
+        RadioMenuItem russianItem = new RadioMenuItem("русский язык");
+        russianItem.setToggleGroup(languageGroup);
+        russianItem.setDisable(true);
+        language.getItems().addAll(chinaItem,englishItem,new SeparatorMenuItem(),russianItem);
+        menuBar.getMenus().add(language);
+        menuBar.setMinHeight(20);
+
+        return menuBar;
+
     }
 
 
@@ -331,7 +392,7 @@ public class Main extends Application {
         hBox1.setSpacing(50);
 
         Separator separator = new Separator();
-        separator.setMaxHeight(1000);
+        separator.setMaxHeight(1200);
 
 
 
@@ -341,6 +402,9 @@ public class Main extends Application {
 
         return vBox;
     }
+
+
+
 
 
 }
