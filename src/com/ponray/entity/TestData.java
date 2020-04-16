43d82d2b9f0bbf8f1d823d53fa96dfbbf9b54e66
zpp360 <184458308@qq.com>
2020-04-16 21:55:49 +0800

    联机

--- src/com/ponray/entity/TestData.java
+++ src/com/ponray/entity/TestData.java
@@ -1,4 +1,82 @@
 package com.ponray.entity;
 
 public class TestData {
+
+    private Long ID;
+
+    private Long testNum;
+
+    private Double timeValue;
+
+    private Float loadVal1;//力1
+    private Float loadVal2;//力2
+    private Float loadVal3;//力3
+
+    private Float posVal;//位移值
+
+    private Float deformVal;//变形值
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public Long getTestNum() {
+        return testNum;
+    }
+
+    public void setTestNum(Long testNum) {
+        this.testNum = testNum;
+    }
+
+    public Double getTimeValue() {
+        return timeValue;
+    }
+
+    public void setTimeValue(Double timeValue) {
+        this.timeValue = timeValue;
+    }
+
+    public Float getLoadVal1() {
+        return loadVal1;
+    }
+
+    public void setLoadVal1(Float loadVal1) {
+        this.loadVal1 = loadVal1;
+    }
+
+    public Float getLoadVal2() {
+        return loadVal2;
+    }
+
+    public void setLoadVal2(Float loadVal2) {
+        this.loadVal2 = loadVal2;
+    }
+
+    public Float getLoadVal3() {
+        return loadVal3;
+    }
+
+    public void setLoadVal3(Float loadVal3) {
+        this.loadVal3 = loadVal3;
+    }
+
+    public Float getPosVal() {
+        return posVal;
+    }
+
+    public void setPosVal(Float posVal) {
+        this.posVal = posVal;
+    }
+
+    public Float getDeformVal() {
+        return deformVal;
+    }
+
+    public void setDeformVal(Float deformVal) {
+        this.deformVal = deformVal;
+    }
 }
